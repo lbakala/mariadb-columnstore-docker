a2c4305d3957c9d37d5bea27f22a5a71e3bac225
Ivan Zlatoustov <ivan.zlatoustov@mariadb.com>
2018-08-21 22:53:39 +0300

    changed to work with the new data format

--- columnstore_zeppelin/multi_node_um1/01_load_bookstore_data.sh
+++ columnstore_zeppelin/multi_node_um1/01_load_bookstore_data.sh
@@ -1,26 +1,35 @@
-#!\bin/sh
-echo "Getting the bookstore data ..."
+#!/bin/sh
 mkdir -p /tmp/bookstore-csv
-if [ ! -f "/docker-entrypoint-initdb.d/sandbox15mr.tar" ]; then
-  curl https://downloads.mariadb.com/sample-data/sandbox15mr.tar --output /docker-entrypoint-initdb.d/sandbox15mr.tar
+if [ ! -f "/docker-entrypoint-initdb.d/sandboxdata.tar" ]; then
+  echo "Getting the bookstore data ..."
+  curl https://downloads.mariadb.com/sample-data/books5000.tar --output /docker-entrypoint-initdb.d/sandboxdata.tar
 fi
 
 echo "Extracting bookstore files ..."
-tar -xf /docker-entrypoint-initdb.d/sandbox15mr.tar --directory /tmp/bookstore-csv
+tar -xf /docker-entrypoint-initdb.d/sandboxdata.tar --directory /tmp/bookstore-csv
 
-echo "loading bookstore files ..."
 # gunzip cover.csv.gz as will use LDI for innodb table later and simplifies
 # for loop below.
 currentDir=$(pwd)
 cd /tmp/bookstore-csv
-gunzip covers.csv.gz
-for i in *.csv.gz; do
+echo "Creating tables ..."
+sed -i 's/%DB%/bookstore/g'  /tmp/bookstore-csv/01_load_ax_init.sql
+/usr/local/mariadb/columnstore/mysql/bin/mysql -u root < /tmp/bookstore-csv/01_load_ax_init.sql
+
+echo "loading bookstore files ..."
+for i in *.mcs.csv.gz; do
     table=$(echo $i | cut -f 1 -d '.')
-    zcat  $table.csv.gz | /usr/local/mariadb/columnstore/bin/cpimport -s ',' -E "'" bookstore $table
-    rm -f $table.csv.gz
+    zcat  $table.mcs.csv.gz | /usr/local/mariadb/columnstore/bin/cpimport -s ',' -E "'" bookstore $table
+    rm -f $table.mcs.csv.gz
 done
 
+for i in *.inno.csv.gz; do
+    gunzip $i
+    table=$(echo $i | cut -f 1 -d '.')
+    /usr/local/mariadb/columnstore/mysql/bin/mysql -u root bookstore -e "load data local infile '$table.inno.csv' into table bookstore.$table fields terminated by ',' enclosed by '''';"
+    rm -f $table.inno.csv.gz
+done
 # now load the covers table which is innodb so use load data local infile
-/usr/local/mariadb/columnstore/mysql/bin/mysql -u root bookstore -e "load data local infile 'covers.csv' into table covers fields terminated by ',' enclosed by '''';"
+cat readme.md
 cd $currentDir
 rm -rf /tmp/bookstore-csv
