62d2fb7f8e07e6bb3dd65d7b7c335774a05b6104
Ivan Zlatoustov <ivan.zlatoustov@mariadb.com>
2019-01-16 02:48:56 +0200

    The footprint of the TravisCI tests is reduced.1. Reduced number of docker images created created by test.2. Removed unused volumes (volume prune)

--- columnstore/test/test.sh
+++ columnstore/test/test.sh
@@ -3,16 +3,20 @@ if [[ "${PWD##*/}" == "test" ]];then
     cd ..
 fi
 . ./test/helpers/testfwk.sh
-export MARIADB_TEST_DEBUG=0
-CONTAINER_NAME="$1"
-CONTAINER_NAME=${CONTAINER_NAME:-"default-container-name"}
-echo "Building $CONTAINER_NAME"
-docker build -t $CONTAINER_NAME . --quiet
-tests+=( "./test/mariadb-docker/run.sh $CONTAINER_NAME" "Test mariadb-docker" )
-#tests+=( "./test/mariadb-sandbox/run.sh $CONTAINER_NAME" "Test mariadb-sandbox" )
+export MARIADB_TEST_DEBUG=
+export MARIADB_CS_DEBUG=
+if [[ ! -z "$1" ]]; then
+    export MARIADB_CONTAINER_NAME="${1}"
+else 
+    export MARIADB_CONTAINER_NAME="mariadb/columnstore:latest"
+fi
+echo "Building ${MARIADB_CONTAINER_NAME}"
+docker build -t ''${MARIADB_CONTAINER_NAME}'' . --quiet
+#tests+=( "./test/mariadb-docker/run.sh $MARIADB_CONTAINER_NAME" "Test mariadb-docker" )
+tests+=( "./test/mariadb-sandbox/run.sh $MARIADB_CONTAINER_NAME" "Test mariadb-sandbox" )
 if [ ${#tests[@]} -gt 0 ]; then
     echo ""
-    echo "Running test suite for ${CONTAINER_NAME}"
+    echo "Running test suite for ${MARIADB_CONTAINER_NAME}"
     echo "----------------------------------------------------------------" 
     start_tst tests[@]
 fi
